ab9e74a0f0a09689f06645b05fc092b9aaf756e2
Soumik Barua <10300902+SoumikBarua@users.noreply.github.com>
2020-10-06 17:37:18 -0400

    Design home tableview cell & add its custom class

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -31,23 +31,23 @@ class HomeTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCell
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetContentLabel.text = "Something else"
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
